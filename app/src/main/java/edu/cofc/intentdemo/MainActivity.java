4df8c61e6bb6419e15847933a29eeeff3451c6a3
kbriggs <kabriggs991@gmail.com>
2017-01-25 22:49:17 -0500

    Implicit Activity

--- app/src/main/java/edu/cofc/intentdemo/MainActivity.java
+++ app/src/main/java/edu/cofc/intentdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.cofc.intentdemo;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,15 +20,22 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.buttonId);
 
         i = new Intent(this, SecondActivity.class);
+        i.putExtra("testString", "This is a test string");
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                //startActivity(i);
+
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://kabriggs.people.cofc.edu"));
                 startActivity(i);
             }
         });
+
+
+
 
 
     }
--- app/src/main/java/edu/cofc/intentdemo/SecondActivity.java
+++ app/src/main/java/edu/cofc/intentdemo/SecondActivity.java
@@ -2,15 +2,25 @@ package edu.cofc.intentdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by kbriggs on 1/25/17.
  */
 public class SecondActivity extends AppCompatActivity {
 
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Bundle extras = getIntent().getExtras();
+        String intentString = extras.getString("testString");
+
+        textView = (TextView) findViewById(R.id.intentText);
+        textView.setText(intentString);
+
     }
 }
