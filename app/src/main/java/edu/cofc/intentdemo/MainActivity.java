580287617b65abdcfece682a404aa4c14c871cd1
kbriggs <kabriggs991@gmail.com>
2017-01-26 12:04:32 -0500

    added implicit and expicit intents

--- app/src/main/java/edu/cofc/intentdemo/MainActivity.java
+++ app/src/main/java/edu/cofc/intentdemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,10 +15,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("onCreate", "**********************************************I made it to onCreate ******************************************");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.buttonId);
+
+        forceError();
 
         i = new Intent(this, SecondActivity.class);
         i.putExtra("testString", "This is a test string");
@@ -38,5 +42,11 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public void forceError() {
+        if(true) {
+            throw new Error("********************** WHOOPS ***********************");
+        }
     }
 }
